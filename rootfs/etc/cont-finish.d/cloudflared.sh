6c3794b59fc2be21e2c58c9e74693599ecde70c8
pptp-plg <pavarut05@hotmail.com>
2022-03-28 08:05:26 +0700

    Revert "major update"
    This reverts commit 7eee2155db495aabebf520843181a284bd474119.

--- rootfs/etc/cont-finish.d/cloudflared.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Configures tunnel
-# ==============================================================================
-
-
-# Backup last version
-cp /usr/local/bin/cloudflared /data/bin/cloudflared
